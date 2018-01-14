625052bfa2032bd70dde0d3ebf209a28d2717696
Joshua Mitchell <jlmitchwd@gmail.com>
2018-01-13 21:01:25 -0500

    Create TimeBox component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import TimeBox from './components/TimeBox';
 import './App.css';
 
 class App extends Component {
+
   render() {
+    // const deadLine = "01/19/2019";
+
     return (
       <div className="App">
-        
+        <TimeBox 
+          increment="" 
+          incrementDescription="" />
+        <TimeBox />
+        <TimeBox />
+        <TimeBox />
       </div>
     );
   }
---
+++ src/components/TimeBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TimeBox = () => {
+    return (
+        <div>
+            <span></span>
+            <div></div>
+        </div>
+    );
+}
+
+export default TimeBox;
